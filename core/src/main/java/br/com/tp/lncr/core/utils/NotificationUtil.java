623bb82db9f1496c1bfe8639c0da21bd7e875399
titoparizotto <titoparizotto@gmail.com>
2025-10-16 20:10:44 -0300

    refactor: Removendo linhas duplicadas.

---
+++ core/src/main/java/br/com/tp/lncr/core/utils/NotificationUtil.java
@@ -0,0 +1,45 @@
+package br.com.tp.lncr.core.utils;
+
+public class  NotificationUtil {
+    public static NotificationData buildNotification(String entity, Integer id, String status) {
+        String notificationType = null;
+        String message = null;
+        String prefixMessage = entity + " com id: ";
+        switch (status.toUpperCase()) {
+            case "RECEIVED":
+                notificationType = entity + "_ORDER_RECEIVED";
+                message = "Pagamento finalizado do " + entity.toLowerCase() + " com id: " + id + ". Aguardando preparo.";
+                break;
+            case "PREPARING":
+                notificationType = entity + "_ORDER_PREPARING";
+                message = prefixMessage + id + " iniciou preparo.";
+                break;
+            case "READY":
+                notificationType = entity + "_ORDER_READY";
+                message = prefixMessage + id + " pronto.";
+                break;
+            case "FINISEHD":
+                notificationType = entity + "_ORDER_FINISHED";
+                message = prefixMessage + id + " finalizado.";
+                break;
+            case "CANCELLED":
+                notificationType = entity + "_ORDER_CANCELLED";
+                message = prefixMessage + id + " cancelado.";
+                break;
+            default:
+                break;
+        }
+        return new NotificationData(notificationType, id, message);
+    }
+
+    public static class NotificationData {
+        public final String type;
+        public final Integer id;
+        public final String message;
+        public NotificationData(String type, Integer id, String message) {
+            this.type = type;
+            this.id = id;
+            this.message = message;
+        }
+    }
+}
